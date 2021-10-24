00d16c28a16249fef06bc4665706a3d9f791dc73
Yusuke Wakui <yusuke.wakui@nextbeat.net>
2021-10-24 11:22:41 +0900

    first commit

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
+import { Photo,PhotoService } from '../services/photo.service';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +9,35 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  constructor(public photoService: PhotoService,
+              public actionSheetController: ActionSheetController) {}
+
+  async ngOnInit() {
+    await this.photoService.loadSaved();
+  }
+
+  async addPhotoToGallery() {
+    await this.photoService.addNewToGallery();
+  }
+
+  public async showActionSheet(photo:Photo, position: number) {
+    const actionSeet = await this.actionSheetController.create({
+      header: 'Photos',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+            this.photoService.deletePicture(photo, position);
+          }
+        },{
+          text: 'Cancel',
+          icon: 'close',
+          role: 'cancel',
+          handler: () => {}
+        }]
+    });
+    await actionSeet.present();
+  }
 
 }
